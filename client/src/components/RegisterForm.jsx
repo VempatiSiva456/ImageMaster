d5969a28b57b34d96f7422dd1588503dcc87995e
VempatiSiva456 <vempati.reddy@students.iiit.ac.in>
2024-05-09 16:28:09 +0530

    error handling

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -94,7 +94,7 @@ export default function SignUp() {
       navigate("/login");
     } catch (error) {
       console.error("Registration error:", error.message);
-      setErrorMessage(error.message);
+      setErrorMessage("Email Already exists");
     }
   };
 
